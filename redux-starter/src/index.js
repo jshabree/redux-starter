c6ec1b0c9c60cbb4684b396a20db6a5bbce9e519
jshabree <jshabree@emich.edu>
2020-11-23 15:49:24 -0500

    working of transform

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+// import { pipe } from "lodash/fp"
+
+let fp= require('lodash/fp');
+
+// import _ from 'lodash';
+
+const _ = require("lodash")
+
+let input = "  JavaScript  ";
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = str => str.trim();
+const wrap = (type, str) => '<${type}>${str}</${type}>';
+const toLowerCase = str => str.toLowerCase();
+
+const transform= fp.pipe(trim, toLowerCase, wrap);
+
+console.log(transform(input));
