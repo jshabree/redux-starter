c29dee298187af840f1f370220271d1e2fea2082
jshabree <jshabree@emich.edu>
2020-11-24 16:02:37 -0500

    arrow operators

--- bug-tracker/src/redux/actions.js
+++ bug-tracker/src/redux/actions.js
@@ -1,10 +1,17 @@
 import * as actions from './actionTypes.js';
 
-export function bugAdded(params) {
-    return {
+export const bugAdded = description => ({
         type: actions.BUG_ADDED,
         payload: {
-            description: 'bug1'
-        }
+        description: 'bug1'
     }
-}
+})
+
+// export function bugAdded(params) {
+//     return {
+//         type: actions.BUG_ADDED,
+//         payload: {
+//             description: 'bug1'
+//         }
+//     }
+// }
