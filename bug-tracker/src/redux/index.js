95a52eeb18ce773dbfbb78b224b11bbe5be7969b
jshabree <jshabree@emich.edu>
2020-11-24 15:48:24 -0500

    exploring the subscribe option

--- bug-tracker/src/redux/index.js
+++ bug-tracker/src/redux/index.js
@@ -1,3 +1,23 @@
 import store from './store.js';
 
-console.log(store);
+const unsubscribe = store.subscribe(() => {
+    console.log("Store changed", store.getState());
+});
+
+
+store.dispatch({
+    type: 'bugAdded',
+    payload: {
+        description: 'bug1'
+    }
+});
+
+unsubscribe();
+
+store.dispatch({
+    type: 'bugRemoved',
+    payload: {
+        id:1
+    }
+})
+console.log(store.getState());
