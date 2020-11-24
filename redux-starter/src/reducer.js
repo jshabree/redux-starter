9b6ded6bd5d042b8d892fd4cb4fa26aec61918f8
jshabree <jshabree@emich.edu>
2020-11-24 14:48:37 -0500

    switch

--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -2,19 +2,21 @@ let lastId = 0; // counter for Id
 
 
 function reducer(state = [], action) {
-    if (action.type == 'bugAdded') {
-        return [
-            ...state,   // copying all the bugs in current state
-            {           // bug object
-                id: ++lastId,
-                description: action.payload.description,
-                resolved: false
-            }
-        ];
+    switch(action.type) {
+        case 'bugAdded':
+            return [
+                ...state,   // copying all the bugs in current state
+                {           // bug object
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ];
+            
+        case 'bugRemoved': 
+            return state.filter(bug => bug.id !== action.payload.id);
+        
+        default :
+            return state;
     }
-    else if (action.type === 'bugRemoved') {
-        return state.filter(bug => bug.id !== action.payload.id);
-    }
-    return state;
-
 }
