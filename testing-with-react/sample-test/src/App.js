a2ab209b8dc460aef32dbf7f3dad7c30b6dddffb
jshabree <jshabree@emich.edu>
2020-12-09 11:24:32 -0500

    initial clock

--- testing-with-react/sample-test/src/App.js
+++ testing-with-react/sample-test/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import ToDo from './components/header/todo'
+import Clock from './components/header/clock'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Clock />
         <ToDo />
         
       </div>
