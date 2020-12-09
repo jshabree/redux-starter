6a2a4373e7abe949e5ce2385d2278aa463616e46
jshabree <jshabree@emich.edu>
2020-12-09 09:50:47 -0500

    todo list

--- testing-with-react/sample-test/src/App.js
+++ testing-with-react/sample-test/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import ToDo from './components/header/test'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        Test
+        
+        <ToDo />
         
       </div>
     )
