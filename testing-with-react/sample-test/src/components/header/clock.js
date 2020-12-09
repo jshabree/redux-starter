e281809ac80d4587bc07528fbfc5080501d2246a
jshabree <jshabree@emich.edu>
2020-12-09 11:32:58 -0500

    self-update next

--- testing-with-react/sample-test/src/components/header/clock.js
+++ testing-with-react/sample-test/src/components/header/clock.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
 
-function Clock(props) {
-    return (
-        <div>
-            <h1> Hello </h1>
-            <h2> It is {props.date.toLocaleTimeString()} </h2>
-        </div>
-    );
+// replacing Clock as a functional component
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+    render() {
+        return (
+            <div>
+                <h1> Hello </h1>
+                <h2> It is {this.state.date.toLocaleTimeString()}   </h2>
+                
+            </div>
+        );
+    }
 }
-
-
-function tick() {
-    ReactDOM.render(
-        <Clock date = {new Date()} />,
-        document.getElementById('root')
-    );
-}
-
-setInterval(tick, 1000);
 
 export default Clock;
