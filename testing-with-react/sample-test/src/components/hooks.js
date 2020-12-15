7ae6f7625d328dcd16882af4aacb1e71837c3a97
jshabree <jshabree@emich.edu>
2020-12-15 16:27:58 -0500

    fixing push refs

---
+++ testing-with-react/sample-test/src/components/hooks.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+
+import '../app.css'
+
+class loginUser extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            info: [
+                
+            ]
+        }
+        
+
+        this.handleOnSubmit = this.handleOnSubmit.bind(this)
+        this.handleOnClick = this.handleOnClick.bind(this)
+    }
+
+        handleOnSubmit = (e) => {
+            e.preventDefault();
+            this.setState({[e.target.name]: e.target.value});
+            try{
+                fetch("https://jsonplaceholder.typicode.com/todos/", {
+                    method: "GET",
+                    headers: {"Content-type": "application/json"}
+                }).then(response => response.json())
+                .then(data=> this.setState({info: data}))
+            }
+            
+            catch {
+                console.log("error")
+
+            }
+
+        }
+    
+        handleOnClick = (e) => {
+            // this.setState([e.target.value] = e.target.value);
+        }
+
+    
+    render() {
+        
+        return (
+            <div className = "outer-class"> 
+
+                <div className = "login">
+                    <Form onSubmit = {this.handleOnSubmit}>
+                        <Form.Group controlId = "username">
+                        <Form.Label> Username </Form.Label>
+                        <Form.Control
+                            autoFocus
+                            type = "username"
+                            value = {this.state.username}
+                        />
+                        </Form.Group>
+
+                        <Form.Group controlId = "password">
+                        <Form.Label> Password </Form.Label>
+                        <Form.Control
+                            autoFocus
+                            type = "password"
+                            value = {this.state.password}
+                        />
+                        </Form.Group>
+                    </Form>
+                
+
+                    <Button block size = "sm" type = "submit" onClick = {this.handleOnSubmit}> Submit </Button>
+
+                    <Button block size = "sm" type = "clear" onClick = {this.handleOnClick}> Clear </Button>
+
+                </div> 
+
+                <div className = "table">
+                    {this.state.info.length > 0 ? 
+                
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>userId</th>
+                                    <th>title</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                { (this.state.info.length > 0) ? this.state.info.map((item, index) => {
+                                return (
+                                        <tr key = {index}>
+                                            <td> {item.id} </td>
+                                            <td> {item.userId}</td>
+                                            <td> {item.title} </td>
+                                        </tr>
+                                    )
+                                }) : null }
+
+                            </tbody>   
+
+                        </table>
+
+                    :  console.log('Not enough data to be fetched') }
+                </div>
+            
+            </div>
+        )
+    }
+
+}
+
+export default loginUser;
