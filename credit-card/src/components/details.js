8c3a4643f6378a0b77df51d00b8d94c2640ff1a5
jshabree <jshabree@emich.edu>
2020-12-16 16:01:57 -0500

    using form control

--- credit-card/src/components/details.js
+++ credit-card/src/components/details.js
@@ -6,38 +6,38 @@ class details extends Component {
             <div className = "form">
             <form className = "details">
 
-            <div className = "form-group">
+            <div className = "form-inline">
 
                 <label for = "ccn"> Card Number </label>
                 <input id = "ccn" type = "tel" inputMode = "numeric" maxlength = "10" class = "form-control" placeholder = "Enter your card number"/>
 
             </div>
 
-            <div className = "form-group">
+            <div className = "form-inline">
 
                 <label for = "month"> MM </label>
-                <input id = "month" placeholder = "Month"/>
+                <input id = "month" class = "form-control" placeholder = "Month"/>
            
                 <label for = "year"> YY </label>
-                <input id = "year" placeholder = "Year"/>
+                <input id = "year" class = "form-control" placeholder = "Year"/>
             
 
                 <label for = "cvv"> CVV </label>
-                <input id = "cvv" placeholder = "Enter your CVV"/>
+                <input id = "cvv" class = "form-control" placeholder = "Enter your CVV"/>
 
             </div>
 
             
-            <div className = "form-group">
+            <div className = "form-inline">
                 <label for = "firstName"> First Name </label>
-                <input id = "firstName" type = "text" placeholder = "Enter your first name"/>
+                <input id = "firstName" type = "text" class = "form-control" placeholder = "Enter your first name"/>
 
                 <label for = "lastName"> Last Name </label>
-                <input id = "lastName" type = "text" placeholder = "Enter your last name"/>
+                <input id = "lastName" type = "text" class = "form-control" placeholder = "Enter your last name"/>
             
             </div>
 
-                <button type = "submit" class = "btn btn-primary" > Place your order</button>
+                <button type = "submit"  class = "btn btn-primary" > Place your order</button>
                
             </form>
 
