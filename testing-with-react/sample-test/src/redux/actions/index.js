4618821c5a1bb2bc7f93740fd955133f07ef06de
jshabree <jshabree@emich.edu>
2020-12-16 10:20:06 -0500

    adding types for actions

---
+++ testing-with-react/sample-test/src/redux/actions/index.js
@@ -0,0 +1,8 @@
+import * as Types from "./types";
+
+export const getData = () => {
+    return {
+        type: Types.GET_DATA
+        // omitting payload
+    };
+};
---
+++ testing-with-react/sample-test/src/redux/actions/types.js
@@ -0,0 +1,3 @@
+export const GET_DATA = "GET_data";
+export const GET_DATA_SUCCESS = "ET_DATA_SUCCESS";
+export const GET_DATA_FAIL = "GET_DATA_FAIL";
