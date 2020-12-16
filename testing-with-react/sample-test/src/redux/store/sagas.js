eaca44cc8f48a64306eacc47bf4eb64de3a14992
jshabree <jshabree@emich.edu>
2020-12-16 11:19:53 -0500

    defining saga declarations

--- testing-with-react/sample-test/src/redux/store/sagas.js
+++ testing-with-react/sample-test/src/redux/store/sagas.js
@@ -11,9 +11,36 @@ import {
 } from "../service";
 
 function* fetchData(action) {
+    try {
+        const baseUrl = "";
+        console.log("Action at sagas =>", JSON.stringify(action));
+        const requestMethod = "";
+        const response = yield call(getDataFromServer, baseUrl, requestMethod, "");
+
+        const result = yield response.json();
+        console.log("Result =>" + JSON.stringify(result));
+        console.log("Result JSON" + result);
+
+        if(result.error) {
+            yield put ({
+                type: "GET_DATA_FAIL",
+                error: result.error
+            });
+        }
+
+        else {
+            yield put ({ type: Types.GET_DATA_SUCCESS, result });
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
 export default function* rootSaga(params) {
-    
+    yield takeLatest(Types.GET_DATA, fetchData);
+
+    console.log("Root Saga");
+
 }
