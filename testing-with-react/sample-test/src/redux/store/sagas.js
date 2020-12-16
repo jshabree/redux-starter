e0bb33eac5166c8f9f4d3c184e70ac0c4adfea55
jshabree <jshabree@emich.edu>
2020-12-16 11:11:45 -0500

    adding generator functions

---
+++ testing-with-react/sample-test/src/redux/store/sagas.js
@@ -0,0 +1,19 @@
+import {
+    call,
+    put,
+    takeLatest
+} from "redux-saga/effects";
+
+import * as Types from "../actions/types";
+
+import {
+    getDataFromServer,
+} from "../service";
+
+function* fetchData(action) {
+
+}
+
+export default function* rootSaga(params) {
+    
+}
