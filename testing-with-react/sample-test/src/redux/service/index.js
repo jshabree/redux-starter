f81aa39cd447e91ffe378320aa62de29c21040d3
jshabree <jshabree@emich.edu>
2020-12-16 10:49:35 -0500

    adding get method to service

--- testing-with-react/sample-test/src/redux/service/index.js
+++ testing-with-react/sample-test/src/redux/service/index.js
@@ -1,7 +1,28 @@
-export function getDataFromServer() {
+export function getDataFromServer(apiPath, requestMethod) {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-type", "application/json");
 
+    if(requestMethod !== 'GET' && requestMethod !== '') {
+        return fetch(apiPath, { method: 'POST', headers: myHeaders });
+    }
+
+    else {
+        return fetch(apiPath, { method: 'GET', headers: myHeaders });
+    }
 }
 
-export function getDataFromServerForPosting() {
-    
+export function getDataFromServerForPosting(apiPath, requestMethod, formBody) {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-type", "application/json");
+
+    if(formBody) {
+        let fetchData = {
+            method: requestMethod,
+            body: formBody,
+            headers: myHeaders
+        };
+
+        return fetch(apiPath, fetchData);
+    }
+
 }
