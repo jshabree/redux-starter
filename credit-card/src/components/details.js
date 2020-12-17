382680c3bd9fcfece16a080efe699f628f6e5757
jshabree <jshabree@emich.edu>
2020-12-17 09:57:05 -0500

    bootstrap attempt at validation

--- credit-card/src/components/details.js
+++ credit-card/src/components/details.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react'
 
 class details extends Component {
+    
+    
+    
+    
     render() {
         return (
             <div className = "center_div">
-            <form className = "details">
+            <form className = "details" noValidate>
 
             <div className = "form-inline">
-                <input id = "ccn" type = "tel" inputMode = "numeric" maxlength = "10" class = "form-control" placeholder = "Enter your card number"/>
+                <input id = "validationCustom01" type = "tel" inputMode = "numeric" maxlength = "10" class = "form-control" placeholder = "Enter your card number*"/>
+                <div class = "invalid-feedback">
+                    Please enter your card details
+                </div>
             </div>
 
             <div className = "row">
                 <div class = "col">
-                    <input id = "month" class = "form-control" placeholder = "Month"/>
+                    <input id = "month" class = "form-control" placeholder = "Month*"/>
                 </div>
 
                 <div class = "col">
-                    <input id = "year" class = "form-control" placeholder = "Year"/>
+                    <input id = "year" class = "form-control" placeholder = "Year*"/>
                 </div>
 
                 <div class = "col">
-                    <input id = "cvv" type = "password" class = "form-control" placeholder = "Enter your CVV"/>
+                    <input id = "cvv" type = "password" class = "form-control" placeholder = "Enter your CVV*"/>
                 </div>
 
                 
@@ -29,17 +36,17 @@ class details extends Component {
             
             <div className = "row">
                 <div class = "col">
-                    <input id = "firstName" type = "text" class = "form-control" placeholder = "Enter your first name"/>
+                    <input id = "firstName" type = "text" class = "form-control" placeholder = "Enter your first name*"/>
                     </div>
 
                     <div class = "col">
-                    <input id = "lastName" type = "text" class = "form-control" placeholder = "Enter your last name"/>
+                    <input id = "lastName" type = "text" class = "form-control" placeholder = "Enter your last name*"/>
                 </div>
                 
             </div>
 
             <div class = "primary-button">
-                <button type = "submit"  class = "btn btn-primary" > Place your order</button>
+                <button type = "submit"  class = "btn btn-primary" > Place your order </button>
             </div>
 
                
