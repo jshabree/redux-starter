85d8ca4619467ca8cb2ecbf57fb54e9d565712ac
jshabree <jshabree@emich.edu>
2020-12-24 09:52:41 -0500

    custom hooks

--- credit-card/src/components/hooks/customHook2.js
+++ credit-card/src/components/hooks/customHook2.js
@@ -1,8 +1,12 @@
 import {useState, useEffect} from 'react'
 
 // useFriendStatus is a custom hook we will reuse at later stages
+// useFriendStatus is used to subscribe to a friend's status to see 
+// if he/she is online
 
-export default function useFriendStatus(friendID) {
+function useFriendStatus(friendID) {
+
+    // defining the hook
 
     const [isOnline, setIsOnline] = useState(null);
 
@@ -21,4 +25,24 @@ export default function useFriendStatus(friendID) {
 
 
     return isOnline;
+}
+
+function FriendStatus(props) {
+    const isOnline = useFriendStatus(props.friend.id);
+
+    if(isOnline === null) {
+        return "Loading....";        
+    }
+    return isOnline ? 'Online' : 'Offline';
+
+}
+
+function FriendLisItem(props) {
+    const isOnline = useFriendStatus(props.friend.id);
+
+    return(
+        <li style = {{ color: isOnline ? 'green' : 'black '}}>
+            {props.friend.name}
+        </li>
+    )
 }
