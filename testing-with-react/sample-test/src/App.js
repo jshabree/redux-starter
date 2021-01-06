55538e9fcd5916f5df98326b9627d4e55a31ee12
jshabree <jshabree@emich.edu>
2021-01-06 10:48:56 -0500

    format

--- testing-with-react/sample-test/src/App.js
+++ testing-with-react/sample-test/src/App.js
@@ -5,17 +5,20 @@ function Todo({ todo, index, completeTodo, deleteTodo}) {
   return(
     <div 
     style = {{textDecoration: todo.isCompleted ? 'line-through' : ''}} 
-    className = "todo">
+    className = "todo" >
       { todo.text}
+
 
       <div>
         <button onClick = {() => completeTodo(index)} >
-         Complete 
-         </button>
-         <button onClick = {() => deleteTodo(index)} >
-         Remove 
-         </button>
+          Complete 
+        </button>
+      
+        <button onClick = {() => deleteTodo(index)} >
+          Remove 
+        </button>
       </div>
+
     </div>
   )
 
